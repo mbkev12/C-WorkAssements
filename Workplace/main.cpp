7949533e2bfae28d4f42d214aa255d353c26fecd
mbkev12 <56396880+mbkev12@users.noreply.github.com>
2021-01-24 21:16:04 +0100

    Update main.cpp

--- Workplace/main.cpp
+++ Workplace/main.cpp
@@ -14,21 +14,21 @@ int main() {
    Numbers Num1;        // Declare Num1 of type Numbers
    Numbers Num2;       // Declare Num2 of type Numbers
 double Calculations = 0.0;
-   // box 1 specification
+  
    Num1.Numone = 5.0;
    Num1.Numtwo = 6.0;
    Num1.Multiplication = 7.0;
 
-   // box 2 specification
+  
    Num2.Numone = 10.0;
    Num2.Numtwo = 12.0;
    Num2.Multiplication = 13.0;
 
-   // volume of box 1
+   
    Calculations = Num1.Numone * Num1.Numtwo * Num1.Multiplication;
    cout << "Calculations of Num1 : " << Calculations <<endl;
 
-   // volume of box 2
+
    Calculations = Num2.Numone * Num2.Numtwo * Num2.Multiplication;
    cout << "Calculations of Num2 : " << Calculations <<endl;
    return 0;
